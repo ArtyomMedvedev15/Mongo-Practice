3c39dc6387a9039c7b36ba373a23b7d7bffc7290
Tim01Bro <arthaos14@gmail.com>
2022-01-03 02:59:34 +0300

    Add custom query

--- src/main/java/com/bookstore/repository/BookRepository.java
+++ src/main/java/com/bookstore/repository/BookRepository.java
@@ -3,6 +3,10 @@ package com.bookstore.repository;
 import com.bookstore.model.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends MongoRepository<Book,String> {
 
+    Optional<Book> findBooksByNameBook(String nameBook);
+    int countBookByAuthorBook(String authorName);
 }
